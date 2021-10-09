470876f3eddcde691b9e6fcdc1abe12c36168e92
Crowdbotics <team@crowdbotics.com>
2021-10-09 19:54:33 +0000

    Update models

--- backend/tracking_dog_31272/wsgi.py
+++ backend/tracking_dog_31272/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tracking_dog_31272.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tracking_dog_31272.settings")
 
 application = get_wsgi_application()
